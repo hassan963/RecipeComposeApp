b1f78ba4cf9d2971b28284f979e683e8d1cf99c5
hassan963 <hassan963@outlook.com>
2022-12-11 22:48:55 +0600

    constrain layout added

--- app/src/main/java/com/jatra/receipeappjetpackcompose/presentation/ui/components/CircularIndeterminateProgressBar.kt
+++ app/src/main/java/com/jatra/receipeappjetpackcompose/presentation/ui/components/CircularIndeterminateProgressBar.kt
@@ -9,6 +9,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintSet
 
 @Composable
 fun CircularIndeterminateProgressBar(isDisplayed: Boolean) {
@@ -22,6 +23,26 @@ fun CircularIndeterminateProgressBar(isDisplayed: Boolean) {
             CircularProgressIndicator(
                 color = MaterialTheme.colors.primary
             )
+        }
+    }
+}
+
+fun myDecoupledConstraints(verticalBias: Float): ConstraintSet {
+    return ConstraintSet {
+        val guideline = createGuidelineFromTop(verticalBias)
+        val progressBar = createRefFor("progressBar")
+        val text = createRefFor("text")
+
+        constrain(progressBar) {
+            top.linkTo(guideline)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        }
+
+        constrain(text) {
+            top.linkTo(progressBar.bottom)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
         }
     }
 }
