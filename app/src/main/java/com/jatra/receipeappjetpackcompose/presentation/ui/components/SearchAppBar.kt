627cd2ec4f31173394c7d660ed5f316b6e6a009e
hassan963 <hassan963@outlook.com>
2022-12-11 22:01:58 +0600

    state hoisting added for recipe list fragment

---
+++ app/src/main/java/com/jatra/receipeappjetpackcompose/presentation/ui/components/SearchAppBar.kt
@@ -0,0 +1,91 @@
+package com.jatra.receipeappjetpackcompose.presentation.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import com.jatra.receipeappjetpackcompose.presentation.ui.recipe_list.FoodCategory
+import com.jatra.receipeappjetpackcompose.presentation.ui.recipe_list.getAllFoodCategories
+
+@Composable
+fun SearchAppBar(
+    query: String,
+    onQueryChanged: (String) -> Unit,
+    onExecuteSearch: () -> Unit,
+    selectedCategory: FoodCategory?,
+    onSelectedCategoryChanged: (String) -> Unit
+) {
+    Surface(
+        modifier = Modifier.fillMaxWidth(),
+        color = Color.White,
+        elevation = 8.dp
+    ) {
+
+        Column {
+
+            Row(modifier = Modifier.fillMaxWidth()) {
+                TextField(
+                    modifier = Modifier
+                        .fillMaxWidth(.9f)
+                        .padding(8.dp),
+                    value = query,
+                    onValueChange = { newValue ->
+                        onQueryChanged(newValue)
+                    },
+                    label = {
+                        Text(text = "Search")
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Text,
+                        imeAction = ImeAction.Search
+                    ),
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Filled.Search,
+                            contentDescription = "search icon"
+                        )
+                    },
+                    textStyle = TextStyle(color = MaterialTheme.colors.onSurface),
+                    colors = TextFieldDefaults.textFieldColors(
+                        backgroundColor = MaterialTheme.colors.surface
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onSearch = {
+                            onExecuteSearch()
+                        }
+                    )
+                )
+            }
+
+            LazyRow {
+                itemsIndexed(
+                    items = getAllFoodCategories()
+                ) { _, category ->
+                    FoodCategoryChip(category = category.value,
+                        isSelected = selectedCategory == category,
+                        onSelectedCategoryChanged = {
+                            onSelectedCategoryChanged(it)
+                        },
+                        onExecuteSearch = {
+                            onExecuteSearch()
+                        })
+                }
+
+            }
+        }
+    }
+}
